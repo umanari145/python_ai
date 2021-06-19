5c5b862e0fde0a257f52084b9a75d2583ff16d5f
umanari145 <umanari145@gmail.com>
2021-06-19 12:05:54 +0900

    lesson1データサンプルを作成

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # python_ai
+
+参考リンク<br>
+
+https://github.com/ghmagazine/python_stat_sample
+
+## ライブラリインストール
+```
+pip install -r requirements.txt
+```
---
+++ lesson1/data_sample.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd 
+
+df = pd.read_csv('students.csv', index_col='生徒番号')
+#一般的な表示
+print("--全体的な表示--")
+print(df)
+print("--特定のカラムのみ表示--")
+print(df['握力'])
+
